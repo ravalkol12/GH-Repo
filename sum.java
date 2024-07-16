dbea1fcc556647ea20aa58d3f50fc65f2efdc935
ravalkol12 <rajesh.ravalkol20@gmail.com>
2024-07-16 19:32:53 +0530

    Github commit

---
+++ sum.java
@@ -0,0 +1,21 @@
+public class SumOfNumbers1  
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, n3 =123, sum;  
+sum = n1 + n2 + n3;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}
+/////////////
+
+public class SumOfNumbers1  
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, n3 =123, sum;  
+sum = n1 + n2 + n3;  
+System.out.println("The sum of numbers is: "+sum);  
+} 
+}
+
